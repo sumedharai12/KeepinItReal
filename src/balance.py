fbe785ab934a98e8d368b4753fb109563aa8042e
Stephen Roy <sroy2@hotmail.com>
2020-04-05 12:31:16 -0400

    adding EDA folder and balance.py placeholder

---
+++ src/balance.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import pandas as pd
+
+def SMOTE():
+    '''Helper procedure to manage dataset class imbalance.
+    @TODO
+    
+    Returns
+    -------
+    Four pandas dataframes:
+        train_X, val_X, train_y, val_y
+    '''
+    pass
